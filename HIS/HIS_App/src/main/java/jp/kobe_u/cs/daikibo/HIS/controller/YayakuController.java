59050872de4e48aaac9c16c18a2e3c0e97f4bc6d
hiro-o <okamotohiro0806@gmail.com>
2023-07-25 21:00:47 +0900

    予約フォームの作成

--- HIS/HIS_App/src/main/java/jp/kobe_u/cs/daikibo/HIS/controller/YayakuController.java
+++ HIS/HIS_App/src/main/java/jp/kobe_u/cs/daikibo/HIS/controller/YayakuController.java
@@ -1,16 +1,25 @@
 package jp.kobe_u.cs.daikibo.HIS.controller;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import jp.kobe_u.cs.daikibo.HIS.entity.Yoyaku;
 import jp.kobe_u.cs.daikibo.HIS.service.YoyakuService;
+import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequiredArgsConstructor
 public class YayakuController {
     @Autowired
     YoyakuService ts;
@@ -22,16 +31,56 @@ public class YayakuController {
 
     @GetMapping("/customer")
     String customer(Model model) {
-
         model.addAttribute(new YoyakuForm());
 
         return "customer";
     }
 
+    @GetMapping("/customer/confirm")
+    public String confirmUserRegistration(
+            Model model,
+            RedirectAttributes attributes,
+            @ModelAttribute @Validated YoyakuForm form,
+            BindingResult bindingResult) {
+
+        // Convert checkInDateInput to Stri
+        // ユーザ情報をModelに追加する
+        model.addAttribute(
+                "name",
+                form.getName());
+        model.addAttribute(
+                "checkInDateInput",
+                form.getCheckInDateInput());
+        // model.addAttribute(
+        // "stayDays",
+        // form.getStayDays());
+        model.addAttribute(
+                "email",
+                form.getEmail());
+        model.addAttribute(
+                "breakfast",
+                form.isBreakfast());
+        // ユーザ登録確認ページ
+        return "customerRegistration";
+    }
+
+    @PostMapping("/customer/register")
+    public String registerUser(
+            Model model,
+            RedirectAttributes attributes,
+            @ModelAttribute @Validated YoyakuForm form,
+            BindingResult bindingResult) {
+
+        ts.createYoyaku(form);
+
+        // 体調記録ページ
+        return "conguratulation";
+    }
+
     @GetMapping("/front")
     String showyoyakuList(Model model) {
         ArrayList<Yoyaku> list = new ArrayList<>();
-        list = ts.getAllYoyaku();
+        // list = ts.getAllYoyaku();
         model.addAttribute("Yoyakulist", list);
         return "front";
     }
--- HIS/HIS_App/src/main/java/jp/kobe_u/cs/daikibo/HIS/repository/YoyakuRepository.java
+++ HIS/HIS_App/src/main/java/jp/kobe_u/cs/daikibo/HIS/repository/YoyakuRepository.java
@@ -1,16 +1,15 @@
 package jp.kobe_u.cs.daikibo.HIS.repository;
 
-import java.time.LocalDate;
-
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import jp.kobe_u.cs.daikibo.HIS.entity.Yoyaku;
 
 @Repository
-public interface YoyakuRepository extends CrudRepository<Yoyaku, Long>{
-    public Iterable<Yoyaku> findByDate(LocalDate date);
-    public Iterable<Yoyaku> findByDateAndBreakfast(LocalDate date, boolean breakfast);
-    public Iterable<Yoyaku> findByDateAndClean(LocalDate date, int roomNumber);
+public interface YoyakuRepository extends CrudRepository<Yoyaku, Long> {
+    // public Iterable<Yoyaku> findByDate(LocalDate date);
+    // public Iterable<Yoyaku> findByDateAndBreakfast(LocalDate date, boolean
+    // breakfast);
+    // public Iterable<Yoyaku> findByDateAndClean(LocalDate date, int roomNumber);
 
 }
